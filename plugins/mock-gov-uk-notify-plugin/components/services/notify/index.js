196423d5da2fa6939923ca719f154c8afcae9b3b
meenahoda <meena.hoda@wmfs.net>
2022-02-08 09:52:15 +0000

    feat: add gov uk notify model and extend send message function

--- plugins/mock-gov-uk-notify-plugin/components/services/notify/index.js
+++ plugins/mock-gov-uk-notify-plugin/components/services/notify/index.js
@@ -1,13 +1,40 @@
 class DummyNotifyService {
   boot (options) {
+    const notificationModel = options.bootedServices.storage.models.tymly_govUkNotifications
+
     this.notifyClient = {}
 
     this.templates = {}
 
-    for (const key of Object.keys(options.blueprintComponents.messageTemplates || {})) {
+    for (const [key, template] of Object.entries(options.blueprintComponents.messageTemplates || {})) {
       this.templates[key] = {
-        sendMessage: () => {
-          console.log('Sending message')
+        sendMessage: async (input) => {
+          input = Array.isArray(input) ? input : [input]
+
+          const notifications = []
+
+          let recipient = ''
+
+          if (template.messageType === 'mail') recipient = input.emailAddress
+          if (template.messageType === 'sms') recipient = input.phoneNumber
+
+          for (const i of input) {
+            const n = {
+              recipient,
+              input: i,
+              template: template.templateId,
+              blueprint: template.blueprintName,
+              templateName: template.name,
+              namespace: template.namespace,
+              notifyId: null,
+              error: null,
+              statusCode: null
+            }
+            notifications.push(n)
+            await notificationModel.create(n)
+          }
+
+          return notifications
         }
       }
     }
