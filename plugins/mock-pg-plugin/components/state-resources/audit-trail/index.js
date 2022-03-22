7af13c40fd1ec92eea18ac9809134ee3e507d552
meenahoda <meena.hoda@wmfs.net>
2022-03-22 15:21:36 +0000

    feat: mock pg plugin with audit trail state resource

---
+++ plugins/mock-pg-plugin/components/state-resources/audit-trail/index.js
@@ -0,0 +1,5 @@
+module.exports = class {
+  run (event, context) {
+    context.sendTaskSuccess([])
+  }
+}
---
+++ plugins/mock-pg-plugin/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  label: 'Dummy PG Plugin',
+  author: 'WMFS',
+  version: '1.0',
+
+  organisation: 'Tymly',
+  description: ''
+}
